2d8a5f79b2e01e332daf81ecd28a48c1849d68dc
Hariharan Devarajan <mani.hariharan@gmail.com>
2024-04-15 15:00:02 -0700

    enable concurrent ds

--- .github/run_hcl.sh
+++ .github/run_hcl.sh
@@ -8,26 +8,11 @@ pushd build
 
 ctest -VV -R benchmark_test || exit 1
 
-# if [ "${HCL_COMMUNICATION}" = "THALLIUM" ]; then
-#     echo "Testing unordered map test"
-#     ctest -V -R ^unordered_map_test || exit 1
-#     echo "Testing unordered map string test"
-#     ctest -V -R ^unordered_map_string_test || exit 1
-#     echo "Testing ordered map"
-#     ctest -V -R ^map_test || exit 1
-#     echo "Testing multimap"
-#     ctest -V -R ^multimap_test || exit 1
-#     echo "Testing priority queue"
-#     ctest -V -R ^priority_queue_test || exit 1
-#     echo "Testing queue"
-#     ctest -V -R ^queue_test || exit 1
-#     echo "Testing set"
-#     ctest -V -R ^set_test || exit 1
-#     echo "Testing concurrent unordered map test"
-#     ctest -V -R ^hashmap_test || exit 1
-#     echo "Testing concurrent queue test"
-#     ctest -V -R ^concurrent_queue_test || exit 1
-#     echo "Testing concurrent skiplist test"
-#     ctest -V -R ^skiplist_test || exit 1
-# fi
+if [ "${HCL_COMMUNICATION}" = "THALLIUM" ]; then
+    ctest -V -R ^hashmap_test || exit 1
+    echo "Testing concurrent queue test"
+    ctest -V -R ^concurrent_queue_test || exit 1
+    echo "Testing concurrent skiplist test"
+    ctest -V -R ^skiplist_test || exit 1
+fi
 popd
