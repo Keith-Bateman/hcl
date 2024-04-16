6c153e12353adbc3c20a907a944f0c96d92043a3
hariharandev1 <hariharandev1@llnl.gov>
2024-04-15 18:04:23 -0700

    clang format check

--- scripts/clang-format-check.sh
+++ scripts/clang-format-check.sh
@@ -7,9 +7,7 @@ find ../test ../include -iname *.h -o -iname *.cpp | xargs clang-format -i -n --
 if [ $? -ne 0 ]; then 
   # The command failed, print an error message 
   echo "The command failed with exit status $?" 
-  # Exit the script with a non-zero exit status to indicate failure 
-  echo "Fixing Errors"
-  find ../test ../include -iname *.h -o -iname *.cpp | xargs clang-format -i --Werror 
+  # Exit the script with a non-zero exit status to indicate failure
   exit 1
 else 
   # The command was successful, print a success message 
