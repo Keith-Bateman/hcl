ba6e93deabec02b6dd747a7c6f96bae3e0d226b8
hariharandev1 <hariharandev1@llnl.gov>
2024-04-15 17:28:37 -0700

    add clang-format check action

---
+++ scripts/clang-format-check.sh
@@ -0,0 +1,17 @@
+#!/bin/bash 
+
+# Run a command 
+find ../test ../include -iname *.h -o -iname *.cpp | xargs clang-format -i -n --Werror 
+ 
+# Check the exit status of the command 
+if [ $? -ne 0 ]; then 
+  # The command failed, print an error message 
+  echo "The command failed with exit status $?" 
+  # Exit the script with a non-zero exit status to indicate failure 
+  echo "Fixing Errors"
+  find ../test ../include -iname *.h -o -iname *.cpp | xargs clang-format -i --Werror 
+  exit 1
+else 
+  # The command was successful, print a success message 
+  echo "The command succeeded with exit status $?" 
+fi 
